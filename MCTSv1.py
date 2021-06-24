a4a09685122cfbf1e905b21ae1d48b89e022b358
enjoy82 <naoya990506@gmail.com>
2021-06-25 02:19:17 +0900

    あとは‘MCTSと自他の分離

--- MCTSv1.py
+++ MCTSv1.py
@@ -4,7 +4,7 @@ from kaggle_environments.envs.hungry_geese.hungry_geese import Observation, Conf
 import random
 import numpy as np
 from collections import defaultdict, deque
-
+import copy
 directions = {0:'EAST', 1:'NORTH', 2:'WEST', 3:'SOUTH', 'EAST':0, 'NORTH':1, 'WEST':2, 'SOUTH':3}
 
 """
@@ -53,87 +53,127 @@ def greedy_choose(head, board):
     return random.randint(0,3)
 """
 
-def playout(state):
-    #報酬値を管理する -> 一番ネック
-    return playout(state.next(randomActions(state)))
-
-def randomActions(state):
-    #すべてのエージェントの合法手をランダムで選択する
-    return 1
 
 #######################　hyper params  ############################
+directdict = {"EAST" : (0, 1), "WEST" : (0, -1), "SOUTH" : (-1, 0), "NORTH" : (1, 0)}
 direct = ["EAST", "WEST", "SOUTH", "NORTH"]
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
 READSTEPS = 8
+NOACTION = "NOACTION"
 ###################################################################
 
 def get1vec(x, y):
-    return x + y * 11
+    return x * 11 + y
 
 def get2vec(s):
-    return s % 11, s // 11
+    #return x, y
+    return s // 11, s % 11
 
-def nextPos(s): #4方向の配列を返す,方角も 0 <= x <= 10 0 <= y <= 6
-    lis = []
-    x, y = get2vec(s)
-    for d, xx, yy in zip(direct, dx, dy):
-        npos = get1vec((x+xx+11)% 11, (y+yy+7)%7)
-        lis.append({"pos":npos, "direct":d})
-    return lis
 
+def playout(state): #TODO　これだと同時着手ゲームに対応できない。自分の手の報酬を管理したい
+    if state.isLose():
+        return -1
+    if state.getReward() != -1:
+        return 0
+    actionlist = []
+    #get randomaction
+    for ind in range(4):
+        actionlist.append(state.randomAction(ind))
+    return playout(state.next(actionlist))
 
 class State:
     def __init__(self, obs):
         #foodの管理
         self.foods = obs.food
         self.step = obs.step
-        self.index = obs.index
+        self.count = 0
+        self.index = obs.index #自分のgeeseのindex
         self.deletion = [False, False, False, False]
-        #当たり判定
-        self.bodyDict = defaultdict(int)
+        #当たり判定　配列で
+        self.board = [[0 for i in range(11)] for l in range(7)]
         #geeseの管理、高速化のためdeque top head
         self.geeses = []
         for _, geese in enumerate(obs.geese):
             deq = deque()
             for s in geese:
-                self.bodyDict[s] = 1
-                deq.append(s)
+                x, y = get2vec(s)
+                self.board[x][y] = 1
+                deq.append((x,y))
             self.geeses.append(deq)
                 
     def checkSegment(self):#step40ごとにsegmentを1削除
         if self.step % 40 == 0:
-            for geese in self.geeses:
+            for ind, geese in enumerate(self.geeses):
+                if self.deletion[ind] == True:
+                    continue
                 geese.pop()
         return
     def checkDeleteGeese(self): #geese削除を管理
         for ind, geese in enumerate(self.geeses):
-            if self.deletion == True:
+            if self.deletion[ind] == True:
+                continue
+            if len(geese) == 0:
+                self.deletion[ind] = True
                 continue
             else:
-                geeseHead = geese[0]
-                if self.bodyDict[geeseHead] >= 2:
+                geeseHeadx, geeseHeady = geese[0]
+                if self.board[geeseHeadx][geeseHeady] >= 2: #重複削除
                     self.deletion[ind] = True
                     for _ in range(len(geese)): #盤面削除
-                        self.bodyDict[geese.pop()] -= 1
+                        x, y = geese.pop()
+                        self.board[x][y] -= 1
 
+        return
+        
+    def next(self, actions): #TODO これ大丈夫か？actionは４手一緒に行う 次の盤面を用意する。TODO #directで管理
+        for ind, action in enumerate(actions):
+            if self.deletion[ind] == True:
+                continue
+            if action == NOACTION:
+                action = directdict[direct[np.random.randint(0, len(4))]] #ランダム行動
+            #TODO write this func!! 頭に入れる！！ けつをカット！！
+        self.checkSegment()
+        self.checkDeleteGeese()
         return 0
-    def next(self, action): #TODO これ大丈夫か？actionは４手一緒に行う
-        return 0
+
     def legalActions(self, ind): #indで指定した合法手(動けるアクション)を取得(もちろん生きているもののみ)
-        geeseHead = self.geeses[0]
-        nextP = nextPos(geeseHead)
+        if self.deletion[ind] == True:
+            return []
+        geeseHeadx, geeseHeady = self.geeses[0]
         nextLegalActions = []
-        for p in nextP:
-            np = p["pos"]
-            if self.bodyDict[np] == 0: #TODO 本当はしっぽいきたい
-                nextLegalActions.append(p)
+        for dir, xx, yy in zip(direct, dx, dy):
+            nx = geeseHeadx + xx
+            ny = geeseHeady + yy
+            if self.board[nx][ny] != 1:
+                nextLegalActions.append(dir)
+            else:
+                for ind, geese in enumerate(self.geeses): #しっぽは最強手
+                    if self.deletion[ind] == True:
+                        continue
+                    geeseBackx, geeseBacky = geese[-1]
+                    if nx == geeseBackx and ny == geeseBacky:
+                        nextLegalActions.append(dir)
+                        break
         return nextLegalActions
-    def isDone(self):
+
+    def randomAction(self, ind):
+        nextLegalActions = self.legalActions(ind)
+        if len(nextLegalActions) == 0:
+            return NOACTION
+        return nextLegalActions[np.random.randint(0, len(nextLegalActions))]
+
+    def isDraw(self):
         return 0
-    def getReward(self): #勝利管理
-        return 0
-    def isLose(self): #敗北
+        
+    def getReward(self): #勝利管理 8手先読み
+        if self.count == READSTEPS:
+            if self.deletion[self.index] == False:
+                return len(self.geeses[self.index]) #ひとまずTODO
+        else:
+            return -1
+
+    def isLose(self): #敗北管理
         if self.deletion[self.index] == True:
             return True
         return False
@@ -141,14 +181,19 @@ class State:
 def mcts_action(state):
     class Node:
         def __init__(self, state):
+            self.state = state
             self.value = 0
             self.w = 0
+            self.child_nodes = None
         def evaluate():
             return 0
         def expand():
             return 0
         def next_child_node():
             return 0
+    
+    root_node = Node(state)
+    root_node.expand()
             
     
 def agent(obs, conf):
